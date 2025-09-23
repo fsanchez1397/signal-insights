0da2f02d161135cf1c08e52f0912951bfb2b1595
fsanchez1397 <felipesanchez10@protonmail.com>
2025-09-23 02:14:10 -0400

    done: add components for LP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import { ThemeProvider } from "./providers/theme-provider";
 
 export const metadata: Metadata = {
 	title: "Create T3 App",
@@ -18,8 +19,16 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html lang="en" className={`${geist.variable}`}>
-			<body>{children}</body>
+		<html lang="en" suppressHydrationWarning className={`${geist.variable}`}>
+			<body>
+				<ThemeProvider attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>
+
+				{children}
+				</ThemeProvider>
+				</body>
 		</html>
 	);
 }
