ab3bb60004b3097edeb2c80397395a7844b5eac5
fsanchez1397 <felipesanchez10@protonmail.com>
2025-09-23 02:38:09 -0400

    fix: add specific type

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,4 +1,9 @@
-const FeatureCard = ({ icon: Icon, title, description }) => {
+type FeatureCardProps = {
+  icon: React.ComponentType<{ size: number }>;
+  title: string;
+  description: string;
+};
+const FeatureCard = ({ icon: Icon, title, description }: FeatureCardProps) => {
   return (
     <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md transition-colors duration-200">
       <Icon size={34} />
